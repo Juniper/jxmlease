7ea346b51724455ca37f2e5ad737ee662a6101f6
Stacy Smith <stacys@juniper.net>
2016-04-07 20:03:19 -0600

    readthedocs.org doesn't support the githubpages extension. Removed it from conf.py since it's not really being used anyway.

--- docs/conf.py
+++ docs/conf.py
@@ -33,7 +33,6 @@ extensions = [
     'sphinx.ext.napoleon',
     'sphinx.ext.ifconfig',
     'sphinx.ext.viewcode',
-    'sphinx.ext.githubpages',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
